4d9d3cc7132042e00e1b17e86bd9af421a959587
ebinthomas01 <ebintkm@gmail.com>
2024-06-08 16:19:58 +0530

    Finised

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const AddAlbum = () => {
+    const [data,setData] =useState(
+        {
+            "UserId":"",
+            "Id":"",
+            "Title":""
+        }
+    )
+
+    const inputHandler = (event) => {
+        setData({...data, [event.target.name]:event.target.value})
+    }
+
+    const readValue = () => {
+        console.log(data)
+    }
+
+
   return (
     <div>
         <NavBar/>
@@ -11,18 +28,18 @@ const AddAlbum = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="label form-label">UserId</label>
-                            <input className="form-control"></input>
+                            <input type='text' className="form-control" name='UserId' value={data.UserId} onChange={inputHandler}></input>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="label form-label">Id </label>
-                        <input className="form-control"></input> 
+                        <label htmlFor="" className="label form-label" >Id </label>
+                        <input type='text' className="form-control"  name='Id' value={data.Id} onChange={inputHandler}></input> 
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="label form-label">Title</label>
-                        <input className="form-control"></input>
+                        <label htmlFor="" className="label form-label" >Title</label>
+                        <input  type='text' className="form-control" name='Title' value={data.Title} onChange={inputHandler} ></input>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-success">Register</button>
+                            <button className="btn btn-success" onClick={readValue}>Register</button>
                         </div>
                     </div>
                 </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const Search = () => {
+    const [data,setData] = useState(
+        {
+            "Album":""
+        }
+    )
+
+    const inputHandler = (event) =>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue = () => {
+        console.log(data)
+    }
+
+
   return (
     <div>
         <NavBar/>
@@ -9,10 +24,10 @@ const Search = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label htmlFor="" className="label form-label">Search Album</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" name='Album' value={data.Album} onChange={inputHandler}/>
                 </div>    
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-primary">Search</button>
+                    <button className="btn btn-primary" onClick={readValue}>Search</button>
                 </div>
             </div>
         </div>
